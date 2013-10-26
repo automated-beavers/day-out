4f49aa7fdb2e115632361490b36975f0280d4e6e
Piotr Murach <pmurach@gmail.com>
2013-10-26 16:07:10 +0100

    Beauty changes.

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-var server = http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 

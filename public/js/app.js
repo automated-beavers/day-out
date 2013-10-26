8849487b9b59681b00d22411e5eee489782455ba
Phil Bottomley <philbottomley1@gmail.com>
2013-10-27 00:21:05 +0100

    Move socket event to global and call inside Angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+var socket = io.connect();
+
 angular.module('routerApp', ['ngRoute']).
 
 config(['$routeProvider', function($routeProvider) {
@@ -13,13 +15,29 @@ controller("game", function () {
 }).
 
 controller("host", function ($scope, $location) {
-  $scope.submit = function(){
-    App.io.emit('createGame', { name: $scope.name });
+  $scope.submit = function () {
+    socket.emit('createGame', { name: $scope.name });
+    $location.path('lobby');
+  };
+
+  socket.on('gameCreated', function (data) {
+    App.players = data.players;
+    App.roomId  = data.roomId;
+  });
+}).
+
+controller('join', function ($scope, $location) {
+  $scope.submit = function () {
+    socket.emit('joinGame', { name: $scope.name, roomId: $scope.roomId });
     $location.path('lobby');
   }
 }).
 
-controller("lobby", function($scope){
+controller("lobby", function ($scope) {
   $scope.players = App.players;
   $scope.roomId = App.roomId;
+
+  socket.on('playerJoined', function (data) {
+    $scope.players = App.players = data.players;
+  });
 });

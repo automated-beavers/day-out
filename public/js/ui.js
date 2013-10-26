7bac0abc619f93fc94f3088914b3f6b1ea931bdb
Phil Bottomley <philbottomley1@gmail.com>
2013-10-26 23:13:58 +0100

    Use proper names for variables

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,26 +1,26 @@
 App.ui = (function () {
 
   var
-    name        = $('#name'),
-    hostGame    = $('#host-game'),
-    joinGame    = $('#join-game'),
-    roomId      = $('#room-id'),
+    $name        = $('#name'),
+    $hostGame    = $('#host-game'),
+    $joinGame    = $('#join-game'),
+    $roomId      = $('#room-id'),
 
   init = function () {
     bindEvents();
   },
 
   bindEvents = function () {
-    hostGame.on('submit', hostGame());
-    joinGame.on('submit', joinGame());
+    $hostGame.on('submit', hostGame);
+    $joinGame.on('submit', joinGame);
   },
 
   hostGame = function () {
-    App.io.emit('createGame', { name: name.val() });
+    App.io.emit('createGame', { name: $name.val() });
   },
 
   joinGame = function () {
-    App.io.emit('joinGame', { name: name.val(), roomId: roomId.val() });
+    App.io.emit('joinGame', { name: $name.val(), roomId: $roomId.val() });
   };
 
   return {

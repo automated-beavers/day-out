3de9c046808ac3f5afc6145ff27ed259151f7748
Darryl Pinto <dazpinto@gmail.com>
2013-10-26 18:46:21 +0100

    add host page

--- public/js/routes.js
+++ public/js/routes.js
@@ -3,6 +3,7 @@ angular.module('routerApp', ['ngRoute']).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', { templateUrl: '/templates/login.html' });
   $routeProvider.when('/game', { templateUrl: '/templates/game.html', controller: "game" });
+  $routeProvider.when('/host', { templateUrl: '/templates/host.html' });
   $routeProvider.otherwise({ redirectTo: '/' });
 }]).
 

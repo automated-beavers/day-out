e553b346b25c5db262bb7d0a1649cddb980fc1c6
Phil Bottomley <philbottomley1@gmail.com>
2013-11-09 18:44:34 +0000

    Refactor room storage

--- engine.js
+++ engine.js
@@ -1,9 +1,7 @@
 var
   io,
   socket,
-  rooms   = [],
-  _       = require('underscore'),
-  colors  = ['yellow', 'red', 'green', 'blue'],
+  Room = require('./room'),
 
 init = function(gameIo, gameSocket){
   io      = gameIo;
@@ -26,29 +24,35 @@ requestSocketId = function () {
 },
 
 createRoom = function (data) {
-  var
-    roomId  = (Math.random() * 100000) | 1,
-    player  = { socketId: this.id, name: data.name };
-    players = newPlayer(roomId, player);
+  var player = {};
+  player.socketId = this.id;
+  player.name     = data.name;
 
-  this.join(roomId);
-  this.emit('roomCreated', { roomId: roomId, players: players });
+  var room = Room.create(player);
+
+  this.join(room.roomId);
+  this.emit('roomCreated', { roomId: room.roomId, players: room.players });
 },
 
 joinRoom = function (data) {
   var
-    roomId  = data.roomId,
-    player  = { socketId: this.id, name: data.name },
-    players = io.sockets.clients(roomId);
+    roomId    = data.roomId,
+    roomCount = io.sockets.clients(roomId).length;
 
-  if(players.length < 4) {
-    var updatedPlayers = newPlayer(roomId, player);
-    this.join(roomId);
-    this.emit('roomJoined', { roomId: roomId, players: updatedPlayers });
-    io.sockets.in(roomId).emit('playerJoined', { players: updatedPlayers })
-  } else {
+  if(roomCount > 4) {
     this.emit('error', { message: 'The room is full' });
+    return;
   }
+
+  var player  = {};
+  player.socketId = this.id;
+  player.name     = data.name;
+
+  var room = Room.join(roomId, player);
+  this.join(roomId);
+
+  this.emit('roomJoined', { roomId: roomId, players: room.players });
+  io.sockets.in(roomId).emit('playerJoined', { players: room.players });
 },
 
 hostStartGame = function (data) {
@@ -56,82 +60,17 @@ hostStartGame = function (data) {
 },
 
 positionCreate = function (data) {
-  if(data.roomId) {
-    var playerPositions = updatePlayerPostion(data.roomId, this.id, data.x, data.y);
-    io.sockets.in(data.roomId).emit('positionUpdate', { players: playerPositions });
+  var roomId = data.roomId;
+
+  if(roomId) {
+    var players = Room.updatePostion(roomId, this.id, data.x, data.y);
+    io.sockets.in(roomId).emit('positionUpdate', { players: players });
   }
 },
 
 finished = function (data) {
-  var winner = findWinner(data.roomId, this.id);
-  io.sockets.in(data.roomId).emit('endGame', { winner: winner })
-},
-
-findWinner = function (roomId, socketId) {
-  var players;
-
-  _.each(rooms, function (item) {
-    if(item.roomId.toString() === roomId.toString()) {
-      players = item.players;
-    }
-  });
-
-  var winner;
-
-  _.each(players, function (item) {
-    if(item.socketId.toString() === socketId.toString()) {
-      winner = item;
-    }
-  });
-
-  return winner;
-},
-
-updatePlayerPostion = function (roomId, socketId, x, y) {
-  var players;
-
-  _.each(rooms, function (item) {
-    if(item.roomId.toString() === roomId.toString()) {
-      players = item.players;
-    }
-  });
-
-  var updateIndex;
-
-  _.each(players, function (item, index) {
-    if(item.socketId.toString() === socketId.toString()) {
-      updateIndex = index;
-    }
-  });
-
-  players[updateIndex].x = x;
-  players[updateIndex].y = y;
-
-  return players;
-},
-
-newPlayer = function (roomId, player) {
-  var roomIndex;
-
-  _.each(rooms, function (item, index) {
-    if(item.roomId.toString() === roomId.toString()) {
-      roomIndex = index;
-    }
-  });
-
-  console.log(roomIndex);
-
-  if(roomIndex !== undefined) {
-    player.color = colors[rooms[roomIndex].players.length];
-    rooms[roomIndex].players.push(player);
-    return rooms[roomIndex].players;
-  }
-  else {
-    player.role = 'host';
-    player.color = colors[0];
-    rooms.push({ roomId: roomId, players: [player] });
-    return [player];
-  }
+  var winner = Room.findPlayer(data.roomId, this.id);
+  io.sockets.in(data.roomId).emit('endGame', { winner: winner });
 };
 
 exports.init = init;
---
+++ room.js
@@ -0,0 +1,98 @@
+module.exports = function () {
+
+  var
+    _       = require('underscore'),
+    rooms   = [],
+    colors  = ['yellow', 'red', 'green', 'blue'],
+
+  create = function (player) {
+    player.role   = 'host';
+    player.color  = _.first(colors);
+
+    var room = {};
+    room.roomId   = uniqueId();
+    room.players  = [player];
+    rooms.push(room);
+
+    return room;
+  },
+
+  join = function (roomId, player) {
+    var index = locate(roomId);
+
+    player.role   = null;
+    player.color  = colors[rooms[index].players.length];
+
+    rooms[index].players.push(player);
+    return rooms[index];
+  },
+
+  updatePostion = function (roomId, socketId, x, y) {
+    var
+      roomIindex  = locate(roomId),
+      players     = rooms[roomIindex].players,
+      index       = locatePlayer(players, socketId);
+
+    players[index].x = x;
+    players[index].y = y;
+
+    return players;
+  },
+
+  findPlayer = function (roomId, socketId) {
+    var
+      roomIindex  = locate(roomId),
+      players     = rooms[roomIindex].players,
+      index       = locatePlayer(players, socketId);
+
+      return players[index];
+  },
+
+  locate = function (roomId) {
+    var index;
+
+    _.each(rooms, function (item, i) {
+      if(item.roomId.toString() === roomId.toString()) {
+        index = i;
+      }
+    });
+
+    return index;
+  },
+
+  locatePlayer = function (players, socketId) {
+    var index;
+
+    _.each(players, function (item, i) {
+      if(item.socketId.toString() === socketId.toString()) {
+        index = i;
+      }
+    });
+
+    return index;
+  },
+
+  uniqueId = function () {
+    var id, existing;
+
+    do {
+      existing  = _.pluck(rooms, 'roomId');
+      id        = randomId();
+    }
+    while (_.contains(existing, id));
+    return id;
+  },
+
+  randomId = function () {
+    return (Math.random() * 10000) | 1;
+  };
+
+
+  return {
+    create: create,
+    join: join,
+    updatePostion: updatePostion,
+    findPlayer: findPlayer
+  };
+
+}();
